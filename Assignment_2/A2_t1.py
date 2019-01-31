ffefbd9193f87d4df25599af80337e538399a189
liaoyuhao <342129019@qq.com>
2019-02-01 06:54:46 +0800

    calculate total values

--- Assignment_2/A2_t1.py
+++ Assignment_2/A2_t1.py
@@ -4,6 +4,15 @@ from numpy import *
 import sys
 import csv
 
+def prepare_data(matrix_data):
+    """reorganize the data in confusion matrix"""
+    data_rows = array(matrix_data[1:])
+    labels, data = hsplit(data_rows, [1])
+    data = data.astype(int)
+    total_predicted = data.sum(axis=1)
+    total_observed = data.sum(axis=0)
+
+    return labels, data, total_predicted, total_observed
 
 if __name__ == "__main__":
     confusion_matrix = sys.argv[1]
@@ -13,5 +22,9 @@ if __name__ == "__main__":
         for row in reader:
             matrix_data.append(row)
 
-    print(matrix_data)
+    labels, data, total_predicted, total_observed = prepare_data(matrix_data)
+    print(labels)
+    print(data)
+    print(total_predicted)
+    print(total_observed)
 
