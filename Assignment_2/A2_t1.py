a27ee4f5fd1d3414dfa97a16250430a98a2aa61f
liaoyuhao <342129019@qq.com>
2019-02-01 09:13:45 +0800

    add calculate_metrics function

--- Assignment_2/A2_t1.py
+++ Assignment_2/A2_t1.py
@@ -4,6 +4,20 @@ from numpy import *
 import sys
 import csv
 
+def calculate_metrics(labels, data, total_predicted, total_observed):
+    """calculate over accuracy, precision, recall, specificity and FDR"""
+    _sum = 0
+    precision, recall, specificity, FDR = [], [], [], []
+    for i in range(len(labels)):
+        _sum += data[i][i]
+        pre = data[i][i] / total_predicted[i]
+        precision.append(pre)
+        obs = data[i][i] / total_observed[i]
+        recall.append(obs)
+    accuracy = _sum / total_predicted.sum()
+
+    return precision, recall, specificity, FDR, accuracy
+
 def prepare_data(matrix_data):
     """reorganize the data in confusion matrix"""
     data_rows = array(matrix_data[1:])
@@ -23,8 +37,11 @@ if __name__ == "__main__":
             matrix_data.append(row)
 
     labels, data, total_predicted, total_observed = prepare_data(matrix_data)
+    calculate_metrics(labels, data, total_predicted, total_observed)
     print(labels)
     print(data)
+    print(data[0][0])
+    print(data[1][1])
     print(total_predicted)
     print(total_observed)
 
