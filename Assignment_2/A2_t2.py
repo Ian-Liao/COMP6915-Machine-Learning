19967ff22045781f1aaebc5ca89e72dd9b37593f
liaoyuhao <342129019@qq.com>
2019-02-10 11:31:27 +0800

    set appropriate thresholds of variance and correlation to filter features

--- Assignment_2/A2_t2.py
+++ Assignment_2/A2_t2.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy import *
+import pandas as pd
 
 import sys
 import csv
@@ -10,11 +11,22 @@ def KFoldCrossValidation(k=5):
 
 
 def prepare_data(train_data):
-    train_data = array(train_data[1:])
-    attributes, labels = hsplit(train_data, [-1])
-    attributes = attributes.astype(float)
-    labels = [label[0] for label in labels]
-    return attributes, labels
+    train_data = array(train_data)
+    # attributes, labels = hsplit(train_data, [-1])
+    train_data = train_data.astype(float)
+    train_data = pd.DataFrame(train_data)
+    print(train_data.shape)
+    var_list, cov_list = [], []
+    feature_num = train_data.shape[1] - 1
+    for i in range(feature_num):
+        corr = train_data[[i, feature_num]].corr()
+        var_list.append((i, train_data[i].var(), corr[feature_num][i]))
+    var_list = [var for var in var_list if var[1] > 1e-4 and abs(var[2]) >= 1e-2]
+    index_set = {var[0] for var in var_list}
+    print("var_list length: ", len(var_list))
+    print(index_set)
+    features = train_data
+    return train_data
 
 if __name__ == "__main__":
     observation_file = sys.argv[1]
@@ -24,6 +36,4 @@ if __name__ == "__main__":
         for row in reader:
             observations.append(row)
 
-    attributes, labels = prepare_data(observations)
-    print(attributes.shape)
-    print(labels)
+    train_data = prepare_data(observations)
