d5a10dc471c559b92e7c5842c842bc180eb9d918
liaoyuhao <342129019@qq.com>
2019-02-11 06:28:42 +0800

    build the k-fold cv structure

--- Assignment_2/A2_t2.py
+++ Assignment_2/A2_t2.py
@@ -6,8 +6,34 @@ import sys
 import csv
 
 
-def KFoldCrossValidation(k=5):
-    pass
+class KFoldCrossValidation(object):
+
+    def split(self, vector, k, index):
+        size = vector.shape[0]
+        start = (size/k) * index
+        end = (size/k) * (index+1)
+        validation = vector[start:end]
+        if str(type(vector)) == "<class 'scipy.sparse.csr.csr_matrix'>":
+            indices = range(start, end)
+            mask = np.ones(vector.shape[0], dtype=bool)
+            mask[indices] = False
+            training = vector[mask]
+        elif str(type(vector)) == "<type 'numpy.ndarray'>":
+            training = np.concatenate((vector[:start], vector[end:]))
+        return training, testing
+
+    def KFoldCrossValidation(self, learner, k, examples, labels):
+        train_folds_score = []
+        validation_folds_score = []
+        for index in range(k):
+            training_set, testing_set = self.split(examples, k, index)
+            training_labels, testing_labels = self.split(labels, k, index)
+            learner.fit(training_set, training_labels)
+            training_predicted = learner.predict(training_set)
+            validation_predicted = learner.predict(validation_set)
+            train_folds_score.append(metrics.accuracy_score(training_labels, training_predicted))
+            validation_folds_score.append(metrics.accuracy_score(validation_labels, validation_predicted))
+        return train_folds_score, validation_folds_score
 
 
 def prepare_data(train_data):
